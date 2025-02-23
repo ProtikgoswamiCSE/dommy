e6b2c24b4ed89f232314e68fb80ef082299a6064
ProtikgoswamiCSE <goseami15-5841@diu.edu.bd>
2025-02-23 15:40:52 +0600

    kjsa

--- hi.c
+++ hi.c
@@ -1,5 +1,6 @@
-int x = 20;
-int y = 18;
-if (x > y) {
-  printf("x is greater than y");
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
 }
